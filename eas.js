52b7b11677538e5f162ce73b77a2777b88e6aedc
RomanFisc <romanfisc@gmail.com>
2023-11-21 17:20:23 -0500

    updated input to allow grid to change size

--- eas.js
+++ eas.js
@@ -1,23 +1,51 @@
-let board = document.querySelector('.board');
+function populateBoard(size){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) =>div.remove());
+    board.style.setProperty('--grid-rows', size);
+    board.style.setProperty('--grid-cols', size);
 
-function makeRows (rows, cols) {
-    board.style.setProperty('--grid-rows', rows);
-    board.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    let amount = size * size;
+    for (c = 0; c < (amount); c++) {
         let cell = document.createElement('div');
         board.appendChild(cell).className = "grid-item";
     };
-};
 
-makeRows(16, 16);
+    var hoverItem = document.getElementsByClassName('grid-item');
 
-var hoverItem = document.getElementsByClassName('grid-item');
-
-for (var i = 0; i < hoverItem.length; i++ ) {
-    hoverItem[i].addEventListener('mouseover', hoverEffect, false);
-}
-function hoverEffect(event) {
-    event.target.style.backgroundColor = 'pink';
+    for (var i = 0; i < hoverItem.length; i++ ) {
+        hoverItem[i].addEventListener('mouseover', hoverEffect, false);
+    }
+    function hoverEffect(event) {
+        event.target.style.backgroundColor = 'pink';
+    }
 }
 
+populateBoard(16);
 
+function changeSize() {
+    let input = document.querySelector('input[type="text"]');
+    let size = parseInt(input.value);
+
+    if (size > 1 && size < 100) {
+        populateBoard(size);
+    } else if (size >= 100) {
+        alert('Please enter a number less than 100');
+    } else {
+        alert('Please enter a valid positive number greater than 1.');
+    }
+}
+
+
+
+
+//Prompt 
+//function gridSet() {
+//    var gridSize = prompt ("Please enter a grid size less than 100");
+//    if (gridSize < 100) {
+//        alert(gridSize);
+//    }
+//    else {
+//        alert("Please enter a number less than 100");
+//    }
+//}   
