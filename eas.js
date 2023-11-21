36121920b6b5246fa3858709b5af526b30052196
RomanFisc <romanfisc@gmail.com>
2023-11-21 15:01:07 -0500

    added a hover effect that follows the mouse path

--- eas.js
+++ eas.js
@@ -11,4 +11,13 @@ function makeRows (rows, cols) {
 
 makeRows(16, 16);
 
+var hoverItem = document.getElementsByClassName('grid-item');
+
+for (var i = 0; i < hoverItem.length; i++ ) {
+    hoverItem[i].addEventListener('mouseover', hoverEffect, false);
+}
+function hoverEffect(event) {
+    event.target.style.backgroundColor = 'pink';
+}
+
 
